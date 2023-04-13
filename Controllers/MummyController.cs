5c9cd83b46d615ae73d697b3412078c6122ee19d
Marcus La Bar <mlabar26@byu.edu>
2023-04-13 17:22:37 -0600

    Add project files.

---
+++ Controllers/MummyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using MummyAnalytics.Data;
+
+namespace MummyAnalytics.Controllers
+{
+    [ApiController]
+    [Route("/score")]
+    public class MummyController : ControllerBase
+    {
+        private InferenceSession _session;
+
+        public MummyController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpPost]
+        public ActionResult Score(MummyPredict data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<float> score = result.First().AsTensor<float>();
+            var prediction = new Prediction { PredictedValue = score.First() * 100000 };
+            result.Dispose();
+            return Ok(prediction);
+        }
+    }
+}
---
+++ Data/Class.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MummyAnalytics.Data
+{
+    public class MummyPredict
+    {
+        public float depth { get; set; }
+        public float headdirection_W { get; set; }
+        public float sex_F { get; set; }
+        public float sex_M { get; set; }
+        public float sex_U { get; set; }
+        public float facebundles_N { get; set; }
+        public float facebundles_Y { get; set; }
+        public float goods_N { get; set; }
+        public float goods_Y { get; set; }
+        public float wrapping_B { get; set; }
+        public float wrapping_H { get; set; }
+        public float wrapping_U { get; set; }
+        public float wrapping_W { get; set; }
+        public float haircolor_A { get; set; }
+        public float haircolor_B { get; set; }
+        public float haircolor_D { get; set; }
+        public float haircolor_K { get; set; }
+        public float haircolor_R { get; set; }
+        public float haircolor_U { get; set; }
+        public float ageatdeath_A { get; set; }
+        public float ageatdeath_C { get; set; }
+        public float ageatdeath_I { get; set; }
+        public float ageatdeath_IN { get; set; }
+        public float ageatdeath_N { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+            depth, headdirection_W, sex_F, sex_M,
+            sex_U, facebundles_N, facebundles_Y, goods_N,
+            goods_Y, wrapping_B, wrapping_H, wrapping_U,
+            wrapping_W, haircolor_A, haircolor_B, haircolor_D,
+            haircolor_K, haircolor_R, haircolor_U, ageatdeath_A,
+            ageatdeath_C, ageatdeath_I, ageatdeath_IN, ageatdeath_N
+            };
+            int[] dimensions = new int[] { 1, 8 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+
+    public class Prediction
+    {
+        public float PredictedValue { get; set; }
+    }
+}
